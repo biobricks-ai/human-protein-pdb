65b9cf8d38a575e4cb0aa5b0fa85533277ff57eb
jpmikhail <mikhailj@mit.edu>
2025-04-29 12:01:59 -0400

    Edited start.sh

--- diffdock_webhook/start.sh
+++ diffdock_webhook/start.sh
@@ -4,9 +4,22 @@ set -e
 # 1) start Redis in the background
 redis-server --daemonize yes
 
-# 2) launch the Celery worker
-#    module: diffdock_docking_service, app instance: celery_app, task queue: default
-celery -A diffdock_docking_service.celery_app worker --loglevel=info &
+# 2) auto-launch one Celery worker per GPU
+#    assumes nvidia-smi is available from your CUDA install
+GPU_COUNT=$(nvidia-smi --query-gpu=index --format=csv,noheader | wc -l)
+for i in $(seq 0 $((GPU_COUNT-1))); do
+  echo "Starting Celery worker on GPU $i"
+  CUDA_VISIBLE_DEVICES=$i \
+    celery -A diffdock_docking_service.celery_app worker \
+      --concurrency=1 \
+      --hostname worker_gpu${i}@%h \
+      --loglevel=info \
+    &
+done
+
+# # 2) launch the Celery worker
+# #    module: diffdock_docking_service, app instance: celery_app, task queue: default
+# celery -A diffdock_docking_service.celery_app worker --loglevel=info --concurrency=1 &
 
 # 3) finally, launch Uvicorn (replacing your old CMD)
 exec uvicorn diffdock_docking_service:app \
